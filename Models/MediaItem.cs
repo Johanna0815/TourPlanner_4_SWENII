a694569bdaf32acb28de3505db200f9dfc178d19
Johanna0815 <81578777+Johanna0815@users.noreply.github.com>
2023-02-24 18:56:23 +0100

    Views and ViewModel blueprint

---
+++ Models/MediaItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TourPlanner_4_SWENII.Models
+{
+    public class MediaItem
+
+    {
+
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ TourPlanner_4_SWENII/ViewModels/MediaFolderVM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TourPlanner_4_SWENII.Models;
+using TourPlanner_4_SWENII.ViewModels;
+
+namespace TourPlanner_4_SWENII.ViewModels
+{
+    public class MediaFolderVM : ViewModelBase
+    {
+
+        private MediaItem currentItem;
+
+
+        private RelayCommand searchCommand;
+        private RelayCommand clearCommand;
+        public ICommand SearchCommand => searchCommand ??= new RelayCommand(Search);
+
+        public ICommand ClearCommand => clearCommand ??= new RelayCommand(Clear);
+        public ObservableCollection<MediaItem> Items { get; set; }
+
+        public MediaItem CurrentItem
+        {
+            get
+            {
+                return currentItem;
+            }
+            set
+            {
+                if ((currentItem != value) && (value != null))
+                {
+                    currentItem = value;
+                    RaisePropertyChangedEvent(nameof(CurrentItem));
+                }
+
+            }
+        }
+
+        public MediaFolderVM()
+        {
+                
+        }
+
+        private void Search(object commandParameter)
+        {
+        }
+
+       
+        private void Clear(object commandParameter)
+        {
+        }
+    }
+}
