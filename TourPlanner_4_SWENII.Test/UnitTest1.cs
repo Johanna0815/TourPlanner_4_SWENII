22b8eb04e051c7e1f9619b25c072a0831375596c
Johanna0815 <81578777+Johanna0815@users.noreply.github.com>
2023-03-10 16:39:21 +0100

    NUnit TestProject added

---
+++ TourPlanner_4_SWENII.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TourPlanner_4_SWENII.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
