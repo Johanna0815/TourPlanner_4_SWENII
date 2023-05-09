ffb46764b810b1808c7a7978fe10c85dc0420464
Johanna0815 <johanna_ist_da@yahoo.de>
2023-05-09 16:30:44 +0200

    log4 net added

---
+++ TourPlanner_4_SWENII/logging/ILoggerWrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace TourPlanner_4_SWENII.logging
+{
+    public interface ILoggerWrapper
+    {
+        void Debug(string message);
+        void Error(string message);
+        void Fatal(string message);
+        void Warn(string message);
+    }
+}
